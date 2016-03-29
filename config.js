e795cc1130ed3780a6e0c3b5e80e9c648ed23c43
路云飞 <luyunfeicx@163.com>
2016-03-29 21:44:01 +0800

    fix appid

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     corpId: 'gh_1c05d6fc17c6'
   },
   mp: {
-    appid: 'gh_1c05d6fc17c6',
+    appid: 'wxd34aa823b8e85243',
     token: 'thuber2016',
     encodingAESKey: 'wjsu9U1frpPXcruhstuH0bB9alrHKBKEGwrw0xX68AC'
   }
