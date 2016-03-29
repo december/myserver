3a80e18f978eec6918d8f4e524e58f291c4b6955
路云飞 <luyunfeicx@163.com>
2016-03-29 20:51:07 +0800

    add token

--- app.js
+++ app.js
@@ -5,6 +5,43 @@ var config = require('./config');
 var mp = require('./controllers/wechat_mp');
 var corp = require('wechat-enterprise');
 
+var url = require("url");
+var crypto = require("crypto");
+
+function sha1(str){
+  var md5sum = crypto.createHash("sha1");
+  md5sum.update(str);
+  str = md5sum.digest("hex");
+  return str;
+}
+
+function validateToken(req,res){
+  var query = url.parse(req.url,true).query;
+  //console.log("*** URL:" + req.url);
+  //console.log(query);
+  var signature = query.signature;
+  var echostr = query.echostr;
+  var timestamp = query['timestamp'];
+  var nonce = query.nonce;
+  var oriArray = new Array();
+  oriArray[0] = nonce;
+  oriArray[1] = timestamp;
+  oriArray[2] = "*********";//这里是你在微信开发者中心页面里填的token，而不是****
+  oriArray.sort();
+  var original = oriArray.join('');
+  console.log("Original str : " + original);
+  console.log("Signature : " + signature );
+  var scyptoString = sha1(original);
+  if(signature == scyptoString){
+    res.end(echostr);
+    console.log("Confirm and send echo back");
+  }else {
+    res.end("false");
+    console.log("Failed!");
+  }
+}
+
+
 var app = connect();
 connect.logger.format('home', ':remote-addr :response-time - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :res[content-length]');
 app.use(connect.logger({
@@ -20,11 +57,13 @@ app.use('/wechat/callback', mp.callback);
 app.use('/wechat', mp.reply);
 app.use('/detail', mp.detail);
 app.use('/login', mp.login);
+
 app.use('/corp', corp(config.corp, function (req, res, next) {
   res.writeHead(200);
   res.end('hello node api');
 }));
 
+app.use('/', validateToken);
 app.use('/', function (req, res) {
   res.writeHead(200);
   res.end('hello node api');
